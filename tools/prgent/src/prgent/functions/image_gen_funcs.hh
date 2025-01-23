acd6035eafc933f2775069f23cde14465c482c21
prothegee <prothegee@proton.me>
2025-01-23 23:39:15 +0700

    adding decode encode barcode qrcode - confirmed Windows

--- tools/prgent/src/prgent/functions/image_gen_funcs.hh
+++ tools/prgent/src/prgent/functions/image_gen_funcs.hh
@@ -6,6 +6,9 @@
 
 #include <prgent/constants/file_extension_const.hh>
 
+#include <filesystem>
+#include <fstream>
+
 #if LIBPRCPP_PROJECT_USING_ZXING
 #include <ZXing/BarcodeFormat.h>
 #include <ZXing/BitMatrix.h>
@@ -15,10 +18,18 @@
 #include <ZXing/MultiFormatWriter.h>
 #include <ZXing/BarcodeFormat.h>
 #include <ZXing/Version.h>
+#include <ZXing/ReadBarcode.h>
+#include <ZXing/ReaderOptions.h>
+#include <ZXing/DecodeHints.h>
+#include <ZXing/ImageView.h>
 
 #if ZXING_EXPERIMENTAL_API
 #include <ZXing/WriteBarcode.h>
 #endif // ZXING_EXPERIMENTAL_API
+
+#if LIBPRCPP_PROJECT_USING_LUNASVG
+#include <lunasvg/lunasvg.h>
+#endif // LIBPRCPP_PROJECT_USING_LUNASVG
 
 using namespace ZXing;
 using namespace libprcpp;
@@ -38,6 +49,18 @@ namespace prgent
  * @return false 
  */
 bool generateSVG(const char *output, const BitMatrix &matrix, const i32 &width, const i32 &height, const i32 &margin);
+
+/**
+ * @brief rasterize bitmap data to output from svgPath param
+ * 
+ * @param svgPath 
+ * @param output 
+ * @param width 
+ * @param height 
+ * @return true 
+ * @return false 
+ */
+bool rasterizeFromSVG(const std::string &svgPath, std::vector<uint8_t> &output, i32 &width, i32 &height);
 
 } // namespace prgent
 
--- tools/prgent/src/prgent/prgent.hh
+++ tools/prgent/src/prgent/prgent.hh
@@ -9,6 +9,10 @@
 #include <prgent/enums/file_extension_enums.hh>
 #include <prgent/enums/generate_enums.hh>
 #include <prgent/enums/return_enums.hh>
+#include <prgent/functions/barcode_funcs.hh>
+#include <prgent/functions/image_gen_funcs.hh>
+#include <prgent/functions/prgent_log_funcs.hh>
+#include <prgent/functions/qrcode_funcs.hh>
 
 // do not inclue main_proc.hh
 
